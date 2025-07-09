c0e31d9ca44e37a675d75a8f1a5a7dd1c0b91129
Moogs <saphielle.akiyama@gmail.com>
2025-07-09 19:35:02 +0900

    No real change tbh

--- Analysis/analyze_df.py
+++ Analysis/analyze_df.py
@@ -16,10 +16,10 @@ def load_df_from_csv(csv_path: list[Path]) -> pd.DataFrame:
 
 if __name__ == "__main__":
     df = load_df_from_csv([*Path("../Ratings").glob("*.csv")])
-    #metrics.plot_context_summary(metrics.summarize_context(df))
+    metrics.plot_context_summary(metrics.summarize_context(df))
     #metrics.plot_metrics_per_category(df)
     #metrics.plot_dual_correlation_matrices(df, method="spearman")
     #metrics.plot_divisiveness_per_rating_percentage(df)
-    metrics.plot_multicriteria_category_representatives(df)
+    #metrics.plot_multicriteria_category_representatives(df)
     #metrics.plot_model_effect_significance(df)
 
